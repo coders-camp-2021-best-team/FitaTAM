50d989165b81d53294750990c93cd43d8c316338
Mariusz Makowski <78812587+Mariusz-web@users.noreply.github.com>
2022-05-03 20:17:40 +0200

    Setup-mui (#13)
    * add mui and config
    
    * add themeprovider and cssbaseline

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,16 +1,22 @@
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './app/app';
+import { theme } from './config/theme';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </StrictMode>
+    <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <StrictMode>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </StrictMode>
+    </ThemeProvider>
 );
