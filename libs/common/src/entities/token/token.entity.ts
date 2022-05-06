e3d5bf6e28ce39c57972d2f1d631718f210153ef
Maciej Opaliński <poprostumieciek@gmail.com>
2022-05-06 09:47:42 +0200

    fix(common): Fix `Token` entity

--- libs/common/src/entities/token/token.entity.ts
+++ libs/common/src/entities/token/token.entity.ts
@@ -2,15 +2,13 @@ import { Column, Entity, ManyToOne, PrimaryColumn } from 'typeorm';
 import { TokenType } from '.';
 import { User } from '../user';
 
-@Entity('user_tokens')
+@Entity('user_token')
 export class Token {
     @PrimaryColumn()
     token: string;
 
     @Column('enum', { enum: TokenType })
     type: TokenType;
-
-    // RELATIONS
 
     @ManyToOne(() => User, (u) => u.tokens, { onDelete: 'CASCADE' })
     user: User;
