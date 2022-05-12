0ba0f7ca184f767ef19259e90ac299152715dd86
Maciej Opaliński <poprostumieciek@gmail.com>
2022-05-12 20:29:14 +0200

    feat(api): Initialize session, redis & passport in `AppModule`

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,12 +1,50 @@
-import { Module } from '@nestjs/common';
+import { Inject, MiddlewareConsumer, Module } from '@nestjs/common';
+import * as passport from 'passport';
+import * as session from 'express-session';
+import * as ConnectRedis from 'connect-redis';
+import { Redis } from 'ioredis';
 
-import { ConfigModule } from '../config';
+import { ConfigModule, ConfigService, NodeEnv } from '../config';
 import { DatabaseModule } from '../database';
+import { AuthModule, CookieExpiryTime } from '../auth';
+import { UserModule } from '../user';
 import { AppController, AppService } from '.';
+import { REDIS } from '../redis';
+
+const RedisStore = ConnectRedis(session);
 
 @Module({
-    imports: [ConfigModule, DatabaseModule],
+    imports: [ConfigModule, DatabaseModule, AuthModule, UserModule],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+    constructor(
+        @Inject(REDIS) private redis_client: Redis,
+        private config: ConfigService
+    ) {}
+
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(
+                session({
+                    secret: this.config.COOKIE_SECRET,
+                    resave: false,
+                    saveUninitialized: false,
+                    cookie: {
+                        maxAge: 7 * CookieExpiryTime.DAY,
+                        httpOnly: true,
+                        secure: 'auto',
+                        sameSite:
+                            this.config.NODE_ENV === NodeEnv.DEVELOPMENT
+                                ? undefined
+                                : 'none',
+                    },
+                    store: new RedisStore({ client: this.redis_client }),
+                }),
+                passport.initialize(),
+                passport.session()
+            )
+            .forRoutes('*');
+    }
+}
