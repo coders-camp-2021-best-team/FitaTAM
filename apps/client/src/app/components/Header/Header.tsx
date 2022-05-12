2fe6a4db61d0be269f55fbd04feb06efef96754d
m.buczek <m.buczek@bsgroup.eu>
2022-05-12 10:26:08 +0200

    refactor: include comments

--- apps/client/src/app/components/Header/Header.tsx
+++ apps/client/src/app/components/Header/Header.tsx
@@ -1,31 +1,48 @@
-import { Settings, SettingsPower } from "@mui/icons-material"
-import { AppBar, Box, IconButton, Typography } from "@mui/material"
-import { NavLink, useNavigate } from "react-router-dom"
-import { ROUTES } from "../../routes/Routes"
+import { Settings, SettingsPower } from '@mui/icons-material';
+import { AppBar, Box, IconButton, Typography } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+import { ROUTES } from '../../routes/Routes';
+import styled from '@emotion/styled';
 
 export const Header = () => {
-  const navigation = useNavigate();
-  const logout = () =>{
-    console.log('you are logged out') //TODO Connect with endpoint for logging out
-  }
-  const goToSettings = () => {
-    navigation(ROUTES.SETTINGS, {replace: true})
-  }
-  return (
-      <AppBar sx={{backgroundColor: 'success.main', height: '64px', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
-        <Typography variant='h5' sx={{color: 'common.white', marginLeft: '20px'}}>
-          <NavLink to={ROUTES.FEED} style={{textDecoration: 'none', color: 'unset'}}>
-            FitaTAM
-          </NavLink>
-        </Typography>
-        <Box>
-          <IconButton size='large' onClick={goToSettings}>
-            <Settings sx={{color: 'common.black'}}/>
-          </IconButton>
-          <IconButton size='large' onClick={logout}>
-            <SettingsPower sx={{color: 'common.black'}}/>
-          </IconButton>
-        </Box>
-      </AppBar>
-  )
-}
+    const logout = () => {
+        console.log('you are logged out'); //TODO Connect with endpoint for logging out
+    };
+    const Header = styled(AppBar)`
+        height: 64px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    `;
+    const Logo = styled(Typography)`
+        margin-left: 20px;
+    `;
+    const Link = styled(NavLink)`
+        text-decoration: none;
+        color: white;
+    `;
+    const BlackSettings = styled(Settings)`
+        color: black;
+    `;
+    const BlackPower = styled(SettingsPower)`
+        color: black;
+    `;
+    return (
+        <Header>
+            <Logo variant='h5'>
+                <Link to={ROUTES.FEED}>FitaTAM</Link>
+            </Logo>
+            <Box>
+                <Link to={ROUTES.SETTINGS}>
+                    <IconButton size='large'>
+                        <BlackSettings />
+                    </IconButton>
+                </Link>
+                <IconButton size='large' onClick={logout}>
+                    <BlackPower />
+                </IconButton>
+            </Box>
+        </Header>
+    );
+};
