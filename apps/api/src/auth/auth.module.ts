86c9466cca115b17d9d0aa5ba85a220349062e3e
Maciej Opaliński <poprostumieciek@gmail.com>
2022-05-14 14:30:12 +0200

    feat: Add remaining functionalities to `AuthModule`
    registering new accounts
    logging out
    activating the account
    requesting a password reset
    resetting a password

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Token, User } from '@fitatam/common';
 import { UserModule } from '../user';
+import { EmailModule } from '../email';
 import { AuthController, AuthService, LocalStrategy, LocalSerializer } from '.';
 
 @Module({
-    imports: [UserModule, PassportModule],
+    imports: [
+        UserModule,
+        PassportModule,
+        TypeOrmModule.forFeature([User, Token]),
+        EmailModule,
+    ],
     providers: [AuthService, LocalStrategy, LocalSerializer],
     controllers: [AuthController],
 })
