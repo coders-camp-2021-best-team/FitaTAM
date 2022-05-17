aed9be69a18966f53b1f59d117449a018a21105b
Maciej Opaliński <poprostumieciek@gmail.com>
2022-05-17 21:48:36 +0200

    feat(api): Add `CaslModule`

---
+++ apps/api/src/casl/casl.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+
+import { CaslAbilityFactory } from '.';
+
+@Module({
+    providers: [CaslAbilityFactory],
+    exports: [CaslAbilityFactory],
+})
+export class CaslModule {}
---
+++ apps/api/src/casl/index.ts
@@ -0,0 +1,5 @@
+export * from './action.enum';
+export * from './subjects.type';
+export * from './app-ability.type';
+export * from './casl-ability.factory';
+export * from './casl.module';
