77c94b5b5c6e6d55ba3e348807f136a044049dd1
Maciej Opaliński <poprostumieciek@gmail.com>
2022-05-17 22:44:18 +0200

    feat(api): Add `PoliciesGuard`

--- apps/api/src/casl/index.ts
+++ apps/api/src/casl/index.ts
@@ -2,4 +2,5 @@ export * from './action.enum';
 export * from './subjects.type';
 export * from './app-ability.type';
 export * from './casl-ability.factory';
+export * from './policies.guard';
 export * from './casl.module';
