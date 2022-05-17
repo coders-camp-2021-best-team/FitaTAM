a57ff902712429ed5ad507d857d06ba530abb331
Maciej Opaliński <poprostumieciek@gmail.com>
2022-05-17 22:44:56 +0200

    feat: Add `/user/:id` endpoint (WIP)

--- apps/api/src/user/user.controller.ts
+++ apps/api/src/user/user.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Param,
+    ParseUUIDPipe,
+    UseGuards,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+
+import { User } from '@fitatam/common';
+import { Action, CheckPolicies, PoliciesGuard } from '../casl';
+import { CookieGuard } from '../auth';
+import { UserService } from '.';
 
 @Controller('user')
-export class UserController {}
+@ApiTags('user')
+@UseGuards(CookieGuard, PoliciesGuard)
+export class UserController {
+    constructor(private userService: UserService) {}
+
+    @Get(':id')
+    @CheckPolicies((a) => a.can(Action.Read, User))
+    getUser(@Param('id', ParseUUIDPipe) id: string) {
+        return this.userService.getByID(id);
+    }
+}
