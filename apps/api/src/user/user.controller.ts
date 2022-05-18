026960ce2c61fe32af6ee9f3a798c203d935d501
Maciej Opaliński <poprostumieciek@gmail.com>
2022-05-18 19:41:01 +0200

    fix(api): Protect `/user/:id` endpoint

--- apps/api/src/user/user.controller.ts
+++ apps/api/src/user/user.controller.ts
@@ -3,24 +3,38 @@ import {
     Get,
     Param,
     ParseUUIDPipe,
+    UnauthorizedException,
     UseGuards,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { User } from '@fitatam/common';
-import { Action, CheckPolicies, PoliciesGuard } from '../casl';
-import { CookieGuard } from '../auth';
+import { Action, CaslAbilityFactory, PoliciesGuard } from '../casl';
+import { CookieGuard, ReqUser } from '../auth';
 import { UserService } from '.';
 
 @Controller('user')
 @ApiTags('user')
 @UseGuards(CookieGuard, PoliciesGuard)
 export class UserController {
-    constructor(private userService: UserService) {}
+    constructor(
+        private userService: UserService,
+        private caslAbilityFactory: CaslAbilityFactory
+    ) {}
 
     @Get(':id')
-    @CheckPolicies((a) => a.can(Action.Read, User))
-    getUser(@Param('id', ParseUUIDPipe) id: string) {
-        return this.userService.getByID(id);
+    async getUser(
+        @Param('id', ParseUUIDPipe) id: string,
+        @ReqUser() requser: User
+    ) {
+        const user = await this.userService.getByID(id);
+
+        if (
+            this.caslAbilityFactory
+                .createForUser(requser)
+                .can(Action.Read, user)
+        ) {
+            return user;
+        } else throw new UnauthorizedException();
     }
 }
