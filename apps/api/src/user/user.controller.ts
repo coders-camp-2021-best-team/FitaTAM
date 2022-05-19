f3906bb751c4d6e1a691165e42ae127c1e00febb
Maciej Opaliński <poprostumieciek@gmail.com>
2022-05-19 10:30:38 +0200

    feat(api): Add update user endpoint
    and properly secure get user endpoint

--- apps/api/src/user/user.controller.ts
+++ apps/api/src/user/user.controller.ts
@@ -1,40 +1,52 @@
 import {
+    Body,
     Controller,
     Get,
     Param,
     ParseUUIDPipe,
+    Patch,
     UnauthorizedException,
     UseGuards,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
-import { User } from '@fitatam/common';
-import { Action, CaslAbilityFactory, PoliciesGuard } from '../casl';
-import { CookieGuard, ReqUser } from '../auth';
+import { UpdateUserDto } from '@fitatam/common';
+import { Action, AppAbility, UserPermissions, PoliciesGuard } from '../casl';
+import { CookieGuard } from '../auth';
 import { UserService } from '.';
 
 @Controller('user')
 @ApiTags('user')
 @UseGuards(CookieGuard, PoliciesGuard)
 export class UserController {
-    constructor(
-        private userService: UserService,
-        private caslAbilityFactory: CaslAbilityFactory
-    ) {}
+    constructor(private userService: UserService) {}
 
     @Get(':id')
     async getUser(
         @Param('id', ParseUUIDPipe) id: string,
-        @ReqUser() requser: User
+        @UserPermissions() perms: AppAbility
     ) {
         const user = await this.userService.getByID(id);
 
-        if (
-            this.caslAbilityFactory
-                .createForUser(requser)
-                .can(Action.Read, user)
-        ) {
-            return user;
-        } else throw new UnauthorizedException();
+        if (!perms.can(Action.Read, user)) {
+            throw new UnauthorizedException();
+        }
+
+        return user;
+    }
+
+    @Patch(':id')
+    async updateUser(
+        @Param('id', ParseUUIDPipe) id: string,
+        @Body() dto: UpdateUserDto,
+        @UserPermissions() perms: AppAbility
+    ) {
+        const user = await this.userService.getByID(id);
+
+        if (!perms.can(Action.Update, user)) {
+            throw new UnauthorizedException();
+        }
+
+        return await this.userService.updateUser(user, dto);
     }
 }
--- apps/api/src/user/user.service.ts
+++ apps/api/src/user/user.service.ts
@@ -1,15 +1,20 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+    Injectable,
+    NotFoundException,
+    UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-import { User } from '@fitatam/common';
+import { UpdateUserDto, User } from '@fitatam/common';
+import { compareSync, hashSync } from 'bcryptjs';
 
 @Injectable()
 export class UserService {
     constructor(@InjectRepository(User) private users: Repository<User>) {}
 
     async getByID(id: string) {
-        const user = this.users.findOne({ where: { id } });
+        const user = await this.users.findOne({ where: { id } });
 
         if (!user) throw new NotFoundException();
 
@@ -17,12 +22,25 @@ export class UserService {
     }
 
     async findOne(email: string) {
-        const user = this.users.findOne({
+        const user = await this.users.findOne({
             where: [{ email }],
         });
 
         if (!user) throw new NotFoundException();
 
         return user;
+    }
+
+    updateUser(user: User, dto: UpdateUserDto) {
+        if (compareSync(dto.current_password, user.password_hash)) {
+            if (dto.password) {
+                user.password_hash = hashSync(dto.password);
+            }
+
+            return this.users.save({
+                ...user,
+                ...dto,
+            });
+        } else throw new UnauthorizedException();
     }
 }
