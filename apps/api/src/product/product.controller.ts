c1dc4b6f83e39b6bf2f63944f4d5aa9d66d4ad27
Konrad <trakon1e@gmail.com>
2022-05-20 19:23:51 +0200

    feat: Create product controller

--- apps/api/src/product/product.controller.ts
+++ apps/api/src/product/product.controller.ts
@@ -1,4 +1,56 @@
-import { Controller } from '@nestjs/common';
+import {
+    CreateProductDto,
+    GetProductDto,
+    UpdateProductDto,
+    UpdateProductStatusDto,
+} from '@fitatam/common';
+import {
+    Body,
+    Controller,
+    Get,
+    HttpCode,
+    Post,
+    Put,
+    Query,
+    Param,
+    Delete,
+} from '@nestjs/common';
+import { ProductService } from './product.service';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+    constructor(private productService: ProductService) {}
+
+    @Get()
+    @HttpCode(200)
+    getProducts(@Query() query: GetProductDto) {
+        return this.productService.getProducts(query);
+    }
+
+    @Post()
+    @HttpCode(201)
+    createProduct(@Body() dto: CreateProductDto) {
+        return this.productService.createProduct(dto);
+    }
+
+    //TODO make this protected - only for author and admin
+    @Put(':id')
+    @HttpCode(200)
+    updateProduct(@Param() params, @Body() dto: UpdateProductDto) {
+        return this.productService.updateProduct(params.id, dto);
+    }
+
+    //TODO admin only
+    @Delete(':id')
+    @HttpCode(200)
+    deleteProduct(@Param() params) {
+        return this.productService.deleteProduct(params.id);
+    }
+
+    //TODO admin only
+    @Post(':id/review')
+    @HttpCode(200)
+    updateProductStatus(@Param() params, @Body() dto: UpdateProductStatusDto) {
+        return this.updateProductStatus(params.id, dto);
+    }
+}
