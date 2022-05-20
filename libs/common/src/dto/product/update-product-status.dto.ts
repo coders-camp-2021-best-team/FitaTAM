7e4ec08a36f21c9198458089c0405893d83f11b6
Konrad <trakon1e@gmail.com>
2022-05-20 20:41:18 +0200

    fix: Fix lint errors

--- libs/common/src/dto/product/update-product-status.dto.ts
+++ libs/common/src/dto/product/update-product-status.dto.ts
@@ -1,8 +1,8 @@
-import { IsEnum, IsOptional } from "class-validator";
-import { ProductStatus } from "../../entities";
+import { IsEnum, IsOptional } from 'class-validator';
+import { ProductStatus } from '../../entities';
 
 export class UpdateProductStatusDto {
     @IsOptional()
     @IsEnum(ProductStatus)
-    status: ProductStatus
+    status: ProductStatus;
 }
