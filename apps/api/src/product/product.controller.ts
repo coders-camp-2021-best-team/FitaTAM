c1a66fc2bb040141035d0b81803cce3dc32715ec
Konrad <trakon1e@gmail.com>
2022-05-23 14:53:45 +0200

    refactor: Remove unnecessary http codes

--- apps/api/src/product/product.controller.ts
+++ apps/api/src/product/product.controller.ts
@@ -7,7 +7,6 @@ import {
     Body,
     Controller,
     Get,
-    HttpCode,
     Post,
     Put,
     Query,
@@ -21,7 +20,6 @@ export class ProductController {
     constructor(private productService: ProductService) {}
 
     @Get()
-    @HttpCode(200)
     getProducts(
         @Query('name') name: string,
         @Query('take') take: number,
@@ -31,28 +29,24 @@ export class ProductController {
     }
 
     @Post()
-    @HttpCode(201)
     createProduct(@Body() dto: CreateProductDto) {
         return this.productService.createProduct(dto);
     }
 
     //TODO make this protected - only for author and admin
     @Put(':id')
-    @HttpCode(200)
     updateProduct(@Param() params, @Body() dto: UpdateProductDto) {
         return this.productService.updateProduct(params.id, dto);
     }
 
     //TODO admin only
     @Delete(':id')
-    @HttpCode(200)
     deleteProduct(@Param() params) {
         return this.productService.deleteProduct(params.id);
     }
 
     //TODO admin only
     @Post(':id/review')
-    @HttpCode(200)
     updateProductStatus(@Param() params, @Body() dto: UpdateProductStatusDto) {
         return this.updateProductStatus(params.id, dto);
     }
